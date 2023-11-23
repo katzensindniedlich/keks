983e9b2916f6be966c6faf8cdcb63d65e911ee20
♥ ck <148710626+katzensindniedlich@users.noreply.github.com>
2023-11-23 18:00:36 +0100

    cli tests and new favicon

---
+++ tests/test_cli.py
@@ -0,0 +1,27 @@
+from typer.testing import CliRunner
+
+import keks
+from keks.__main__ import app
+
+
+runner = CliRunner()
+
+def run(*args):
+    return runner.invoke(app, args)
+
+
+def test_help():
+    result = run('--help')
+    stdout = result.stdout
+
+    assert not result.exit_code
+    assert 'Usage:' in stdout
+    assert '--help' in stdout
+    assert 'Show this message and exit.' in stdout
+
+
+def test_version():
+    result = run('--version')
+
+    assert not result.exit_code
+    assert keks.__version__ in result.stdout
